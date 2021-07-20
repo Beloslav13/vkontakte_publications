8d19adda54d8935e9e737f00a3983bbcc035d02a
beloslav13 <velislav1329@gmail.com>
2021-07-20 21:05:19 +0300

    Базовые настройки

---
+++ docker/postgres/init.sql
@@ -0,0 +1,4 @@
+CREATE USER admin WITH PASSWORD 'devpass';
+
+CREATE DATABASE vkontakte_publications_db;
+GRANT ALL PRIVILEGES ON DATABASE vkontakte_publications_db TO admin;
--- vkontakte_publications/settings.py
+++ vkontakte_publications/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'vkontakte_publications.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('NAME_DB'),
+        'USER': os.environ.get('USER_DB'),
+        'PASSWORD': os.environ.get('PASSWORD_DB'),
+        'HOST': os.environ.get('HOST_DB'),
+        'PORTS': 5432
     }
 }
 
